b35945dc92d493d5aeb0d449a4d0fa592eda33f2
Julien Jerphanion <git@jjerphan.xyz>
2023-02-02 17:33:10 +0100

    ASV Add a simple ASV benchmark for KNeighborsClassifier.predict
    This adds benchmarks for:
    https://github.com/scikit-learn/scikit-learn/pull/24076

---
+++ asv_benchmarks/benchmarks/bruteforce.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+
+from .common import Benchmark
+
+N_SAMPLES_TRAIN = [10_000, 100_000, 1_000_000]
+N_SAMPLES_TEST = [1000, 10_000, 100_000]
+N_FEATURES = [50, 100, 500]
+
+
+class BruteForceKNeighborsClassifier(Benchmark):
+    """Benchmarks for KNeighborsClassifier.predict when using algorithm='brute'."""
+
+    param_names = ["n_train", "n_test", "n_features"]
+    params = (
+        N_SAMPLES_TRAIN,
+        N_SAMPLES_TEST,
+        N_FEATURES,
+    )
+
+    def setup(self, *params):
+        n_train, n_test, n_features = params
+        self.knc = KNeighborsClassifier(
+            n_neighbors=10, algorithm="brute", metric="manhattan"
+        )
+
+        self.rng = np.random.RandomState(0)
+        self.X_train = self.rng.random_sample((n_train, n_features))
+        self.X_test = self.rng.random_sample((n_test, n_features))
+        self.y = self.rng.randint(low=0, high=2, size=(n_train,))
+
+        self.knc.fit(X=self.X_train, y=self.y)
+
+    def time_predict_proba(self, *params):
+        self.knc.predict_proba(self.X_test)
